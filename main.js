a133d7c6f4ab6524c88ce1912ca86be8a48b8974
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-06-13 21:03:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+//Task 26: Alien Colors #2: Choose a color for an alien as you did in Exercise 25,
+// and write an if-else chain.
+//• If the alien’s color is green, print a statement that the player 
+//just earned 5 points for shooting the alien.
+//• If the alien’s color isn’t green, print a statement that the player 
+//just earned 10 points.
+//• Write one version of this program that runs the if block and 
+//another that runs the else block.
+//allien color is green
+let allien_color = "Green";
+if (allien_color == "Green") {
+    console.log("Player just earned 5 points for shooting the allien");
+}
+else {
+    console.log("Player just earned 10 points");
+}
+//allien color is red
+let allien_color2 = "Red";
+if (allien_color2 == "Green") {
+    console.log("Player just earned 5 points for shooting the allien");
+}
+else {
+    console.log("Player just earned 10 points");
+}
